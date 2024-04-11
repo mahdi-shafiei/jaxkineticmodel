66f921b00a670caa5d68c15bc82c200828679553
Paul_v_Lent <P.H.vanLent@tudelft.nl>
2024-04-11 17:56:54 +0200

    reimplemented loadsbml

--- functions/load_sbml/tellurium_test.py
+++ functions/load_sbml/tellurium_test.py
@@ -1,7 +1,6 @@
+
 import tellurium as te
 
 x=te.loadSBMLModel("BIOMD0000000244_url.xml")
-# x.simulate(0,10,100)
-# x.plot()
-x.getNumGlobalParameters()
-x.getReactionRates()
+x.simulate(0,10,100)
+x.plot()
